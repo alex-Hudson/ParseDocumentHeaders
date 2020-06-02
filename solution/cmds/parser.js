c6c93b30d6dd2e76090f737f7f98c545433fffb5
Alex Hudson <hudsonj.alex@gmail.com>
2020-06-02 14:58:40 +0100

    Few minor tweeks

--- solution/cmds/parser.js
+++ solution/cmds/parser.js
@@ -2,15 +2,23 @@ import * as path from "path";
 import * as fs from "fs";
 
 export default class Parser {
-  constructor(args) {
-    const filePath = args;
+  /**
+   * Class to parse file from "files" folder
+   * Data can then be acessed from the data object
+   * @param {Array} filePath command line arguments containing the pa
+   */
+  constructor(filePath) {
     const fileName = path.basename(filePath);
     console.log(`File name is ${fileName}`);
-    try {
-      const jsonPath = path.join(path.resolve(), "files", fileName);
-      this.data = JSON.parse(fs.readFileSync(jsonPath, "utf8"));
-    } catch (e) {
-      console.error(e);
-    }
+
+    this.data = this.loadFile(filePath);
+  }
+
+  /**
+   * Returns Parsed JSON data from file in "files" directory
+   * @param {string} fileName
+   */
+  loadFile(filePath) {
+    return JSON.parse(fs.readFileSync(filePath, "utf8"));
   }
 }
--- solution/cmds/reader.js
+++ solution/cmds/reader.js
@@ -1,28 +1,36 @@
 export default class Reader {
+  /**
+   * Class filter passed paragraph data into headers
+   * @param {Array.<Object>} data array of paragraph objects
+   */
   constructor(data) {
+    this.paragraphs = data;
     this.headers = this.readHeaders(data);
   }
 
   readHeaders(paragraphs) {
     const headers = [];
     paragraphs.forEach((paragraph) => {
-      if (this.isHeader(paragraph.p_text)) headers.push(paragraph.p_text);
+      if (this.isHeader(paragraph.p_text)) headers.push(paragraph);
     });
 
     return headers;
   }
 
   /**
+   * Returns true if string is header else
+   * Conditions for being a header:
    *
    * @param {string} text
+   * @returns {boolean}
    */
   isHeader(text) {
     const textArray = text.split(" ");
+    //longer than 15 words - unlikely to be title
     if (textArray.length > 15) return false;
-    else {
-      if (this.isMajorVersion(textArray)) return true;
-      if (this.isAllCaps(textArray)) return true;
-    }
+    if (!this.containsLetters(text)) return false; //No letters in string
+    if (this.isMajorVersion(textArray)) return true;
+    if (this.isAllCaps(text) && this.isUnique(text)) return true;
   }
 
   /**
@@ -36,7 +44,31 @@ export default class Reader {
 
   /**
    * Returns true if all letters in the array are caps
-   * @param {Array} textArray 
+   * @param {string} text
    */
-  isAllCaps(textArray)
+  isAllCaps(text) {
+    return text === text.toUpperCase();
+  }
+
+  /**
+   * Check if string is unique in this.paragraphs object
+   * @param {string} text
+   */
+  isUnique(text) {
+    const strings = Object.values(this.paragraphs).filter(
+      (paragraph) => paragraph.p_text === text
+    );
+    if (strings.length > 1) return false;
+    else return true;
+  }
+
+  /**
+   * Returns true if string contains letters returns false
+   * If string contains only numbers
+   * @param {string} text
+   */
+  containsLetters(text) {
+    if (text.match(/[a-z]/i)) return true;
+    else return false;
+  }
 }
